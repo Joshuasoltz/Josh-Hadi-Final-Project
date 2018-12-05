8b20cf99cd4030ffe6b45c7d7d6d1dda9f3b2f6c
Joshua Soltz <joshua@Rons-MacBook-Air.local>
2018-12-05 11:40:32 -0800

    -a

--- SneakerBot.js
+++ SneakerBot.js
@@ -1,7 +1,13 @@
 var page = require('webpage').create();
-page.open('https://www.nike.com/launch/t/air-jordan-12-retro-gym-red-black/', function(status) {
+page.open('https://www.nike.com/us/en_us/', function(status) {
   console.log("Status: " + status);
   if(status === "success") {
+    let listWithLogInButton = page.querySelector("li[js-hook='exp-join-login']");
+    
+    
+    console.log("here");
+    let button= listWithLogInButton.getElementsByTagName('button')[0];
+    button.click()
     page.render('example.png');
   }
   phantom.exit();
